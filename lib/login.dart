31013ab54978a694ef7ea795d7a24f2195df745f
Melaniii25 <melanisulistyowati25@gmail.com>
2025-04-30 18:50:55 +0700

    memperbaiki proifl

--- lib/login.dart
+++ lib/login.dart
@@ -1,67 +1,82 @@
 import 'package:flutter/material.dart';
 import 'profill_screen.dart';
 
-// Halaman Login
 class LoginHalaman extends StatelessWidget {
   LoginHalaman({super.key});
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final ValueNotifier<bool> _obscureTextNotifier = ValueNotifier<bool>(true); //untuk hide/show passsword//
+  final ValueNotifier<bool> _obscureTextNotifier = ValueNotifier<bool>(true); 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('HALAMAN LOGIN', style: TextStyle(fontSize: 20),),
-    backgroundColor: Colors.greenAccent,
-    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HALAMAN LOGIN', style: TextStyle(fontSize: 18)),
+        backgroundColor: const Color.fromARGB(255, 162, 202, 214),
+      ),
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-               const Icon(
+              const Icon(
                 Icons.lock_person_rounded,
                 size: 150,
-                color: Colors.greenAccent,
+                color: Color.fromARGB(255, 162, 202, 214),
               ),
               const SizedBox(height: 40),
               TextField(
                 controller: emailController,
                 decoration: InputDecoration(
                   labelText: 'Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12), ),
-                      prefixIcon: const Icon(Icons.email_outlined), ),
-                    keyboardType: TextInputType.emailAddress,),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), ),
+                  prefixIcon: const Icon(Icons.email_outlined),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
               const SizedBox(height: 20),
-              ValueListenableBuilder<bool>(
-                valueListenable: _obscureTextNotifier, builder: (context, obscureText, _) {
-                   return TextField(controller: passwordController, decoration: InputDecoration(
-                     labelText: 'Password',
-                     border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), ),
-                       prefixIcon: const Icon(Icons.lock_outline),
-                        suffixIcon: IconButton(
-                          icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility,),
-                          onPressed: () { _obscureTextNotifier.value = !obscureText;}, ),
-      ),obscureText: obscureText,
-    );
-    },
-   ),
-          const SizedBox(height: 30),
-            SizedBox(width: double.infinity,height: 50,
-            child: ElevatedButton(
-              onPressed: () {
-                String email = emailController.text.trim();
-                String password = passwordController.text.trim(); Navigator.of(context).pushReplacement
-                (MaterialPageRoute(builder: (context) => ProfillScreen( email: email,password: password,)),
-                 );
+              ValueListenableBuilder<bool>( valueListenable: _obscureTextNotifier, builder: (context, obscureText, _) {
+                  return TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),),
+                      prefixIcon: const Icon(Icons.lock_outline),
+                      suffixIcon: IconButton(
+                        icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility,),
+                        onPressed: () {_obscureTextNotifier.value = !obscureText;},
+                      ),
+                    ),
+                    obscureText: obscureText,
+                  );
+                },),
+            const SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    String email = emailController.text.trim();
+                    String password = passwordController.text.trim();
+                    if (!email.endsWith('@gmail.com')) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('')),
+                      );
+                      return;
+                    }
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => ProfileScreen(email: email, password: password,),),
+                    );
                   },
                   style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
+                    backgroundColor: Color.fromARGB(255, 162, 202, 214),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12),),
                   ),
-                  child: const Text('Login', style: TextStyle(fontSize: 18),),
+                  child: const Text('Login',  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black38) ),
                 ),
               ),
             ],
--- lib/profill_screen.dart
+++ lib/profill_screen.dart
@@ -1,7 +1,13 @@
+import 'package:appmobille2/login.dart';
 import 'package:flutter/material.dart';
 
-class ProfillScreen extends StatelessWidget {
-  const ProfillScreen({super.key, required this.email, required this.password});
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({
+    super.key,
+    required this.email,
+    required this.password,
+  });
+
   final String email;
   final String password;
 
@@ -9,30 +15,70 @@ class ProfillScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Halaman Profil ', style: TextStyle(fontSize: 20)),
-        backgroundColor: Colors.greenAccent
+        title: Text('Halaman Profil', style: TextStyle(fontSize: 18)),
+        backgroundColor: Color.fromARGB(255, 162, 202, 214),
+        leading: IconButton(onPressed: (){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginHalaman()));
+        }, icon: Icon(Icons.arrow_back)),
       ),
-     body: Padding(
+      body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
-              child: Text('SELAMAT DATANG PADA HALAMAN PROFIL test', 
-                style: TextStyle(fontSize: 30, fontWeight:FontWeight.bold),
-                textAlign:TextAlign.center,),
-            ), 
-            Text('Email:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            Text(email, style: TextStyle(fontSize: 16)),
-            const SizedBox(height: 24),
-            Text('Password:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            Text(password, style: TextStyle(fontSize: 16)),
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromARGB(255, 162, 202, 214),
+                ),
+                child: Icon(
+                  Icons.person,
+                  size: 70,
+                  color: Colors.white,
+                ),
+              ),
+            ),
             const SizedBox(height: 40),
-          ], 
-    ),
-     ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.email, color: Color.fromARGB(255, 52, 101, 116)),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Email:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+                      SizedBox(height: 4),
+                      Text(email, style: TextStyle(fontSize: 16), textAlign: TextAlign.center,),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.lock, color: Color.fromARGB(255, 52, 101, 116)),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Password:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 4),
+                      Text(password, style: TextStyle(fontSize: 16)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
